ccc6be7d031687ef92c98d9ca913953639061d77
Miilaenh <110079781+Miilaenh@users.noreply.github.com>
2023-03-03 07:38:28 -0600

    Actualizacion de recuperar
    Se añaido el boton y se corrigieron errores en la interfaz

--- lib/src/screens/recuperar.dart
+++ lib/src/screens/recuperar.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
-
 class Recuperar extends StatelessWidget {
   const Recuperar({super.key});
 
   @override
-  Widget build(context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Recuperacion de contraseña"),
-      ),
-      body:const Center(
-          child: Column(
-            children: <Widget>[
-              Text("Recuperar Contraseña"),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Correo",
-                ),
-              ),
-            ],
-          ) 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Unique Systems'),
         ),
+        body: Center(
+            child: Column(
+              children: [
+                Text(
+                  "Recuperar contraseña".toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                _textFieldEmail(),
+                SizedBox(
+                  height: 15.0,
+                ),Padding(
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () => null,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      minimumSize: const Size(
+                        double.infinity,
+                        60,
+                      ),
+                    ),
+                    child: const Text("Recuperar"),
+                  ),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
+
+Widget _textFieldEmail() {
+  return Container(
+    margin: const EdgeInsets.symmetric(
+      horizontal: 30.0,
+    ),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    // ignore: prefer_const_constructors
+    child: TextField(
+      decoration: const InputDecoration(
+        prefixIcon: Icon(Icons.email_outlined),
+        labelText: 'Correo Electronico',
+        hintText: 'example@test.com',
+      ),
+      onChanged: (value) {},
+    ),
+  );
+}
+
+
